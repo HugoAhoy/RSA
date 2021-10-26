554fe0fdb5b03d45ce42993a6b3a9916b9486ae2
HugoAhoy <zhjd3302@outlook.com>
2021-10-26 08:29:27 +0800

    add some constructors and some necessary private variables

--- include/Int.hpp
+++ include/Int.hpp
@@ -2,11 +2,16 @@
 #ifndef INT_HPP
 #define INT_HPP
 #include "string"
+#include "vector"
+#include "cmath"
+
 class Int{
     public:
     Int(std::string val);
 
     Int(int val);
+    
+    Int(long long val);
     
     std::string val();
     // Int operator+(const Int& b);
@@ -16,7 +21,13 @@ class Int{
     // Int mod(const Int& b);
 
     private:
-    std::string _val;
+    bool _is_positive;
+    std::vector<long long> _val;
+    static const long long _unit_length = 18;
+    long long _units;
+    static const long long _BASE = static_cast<long long>(1e18);
+
+    bool is_str_legal(const std::string &val);
 };
 #endif
 
