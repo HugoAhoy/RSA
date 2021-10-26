dc9e11c3ed5808ea5de8823a4c3a22a5a1904311
HugoAhoy <zhjd3302@outlook.com>
2021-10-26 08:38:41 +0800

    add some test cases for Int_test.BasicAssertions

--- tests/Int_test.cpp
+++ tests/Int_test.cpp
@@ -1,7 +1,14 @@
-#include"Int.hpp"
+#include "Int.hpp"
+#include "string"
 #include "gtest/gtest.h"
 
 TEST(IntTest, BasicAssertions) {
   // Expect two strings not to be equal.
   EXPECT_EQ("1233", Int("1233").val());
+  EXPECT_EQ("2531564864864185005455884", Int("2531564864864185005455884").val());
+  EXPECT_EQ("-1864868", Int("-1864868").val());
+  EXPECT_EQ(std::to_string(519854864), Int(519854864).val());
+  EXPECT_EQ(std::to_string(-346846), Int(-346846).val());
+  EXPECT_EQ(std::to_string(78541274693145ll), Int(78541274693145ll).val());
+  EXPECT_EQ(std::to_string(-21542365237ll), Int(-21542365237ll).val());
 }
