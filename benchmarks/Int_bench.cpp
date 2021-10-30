da3b71f6ace67f0876eead1a22ddd8210407032d
HugoAhoy <zhjd3302@outlook.com>
2021-10-30 23:53:53 +0800

    add benchmark for div(now implemented by newton method and binary search) and div by pure binary search

--- benchmarks/Int_bench.cpp
+++ benchmarks/Int_bench.cpp
@@ -14,3 +14,30 @@ static void Int_mul(benchmark::State& state) {
     }
 }
 BENCHMARK(Int_mul);
+
+// 大数除法
+static void Int_div(benchmark::State& state) {
+    Int a, b, res;
+    for (auto _ : state){
+        state.PauseTiming();
+        a = Random_Nbits_Int(2048);
+        b = Random_Nbits_Int(1024);
+        state.ResumeTiming();
+        a/b;
+    }
+}
+BENCHMARK(Int_div);
+
+
+static void Int_div_bs(benchmark::State& state) {
+    Int a, b, res;
+    for (auto _ : state){
+        state.PauseTiming();
+        a = Random_Nbits_Int(2048);
+        b = Random_Nbits_Int(1024);
+        state.ResumeTiming();
+        Int::div_by_binary_search(a,b);
+    }
+}
+BENCHMARK(Int_div_bs);
+
