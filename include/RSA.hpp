7b83b2517a47a1090cb162f456fe08b57a6a3e75
HugoAhoy <zhjd3302@outlook.com>
2021-11-02 19:33:20 +0800

    ADD declaration of struct Keys. CHANGE declaration of GenerateKey()

--- include/RSA.hpp
+++ include/RSA.hpp
@@ -1,10 +1,17 @@
-// Int.hpp
+// RSA.hpp
 #ifndef RSA_HPP
 #define RSA_HPP
 #include "Int.hpp"
 #include "string"
 
-std::pair<Int, Int> GenerateKey(Int KeyLength);
+struct Keys{
+    Int n;
+    Int e; // 公钥
+    Int d; // 私钥
+    Keys(const Int &_n, const Int &_e, const Int &_d);
+};
+
+Keys GenerateKey(long long key_length);
 
 Int Encrypty(Int PublicKey, Int Content);
 
