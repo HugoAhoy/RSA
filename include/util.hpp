2c448f91dd664fe2bafcfe2d8df7c92d2dbdb380
HugoAhoy <zhjd3302@outlook.com>
2021-11-02 19:32:23 +0800

    ADD defensive declaration

--- include/util.hpp
+++ include/util.hpp
@@ -1,5 +1,12 @@
+// util.hpp
+#ifndef UTIL_HPP
+#define UTIL_HPP
 #include "Int.hpp"
 
 Int Random_Nbits_Int(long long nbits);
 
+Int Random_Nbits_Prime(long long nbits);
+
 bool Miller_Rabin(const Int &b);
+
+#endif
