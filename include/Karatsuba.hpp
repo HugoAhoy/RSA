98acb08642c1913ab18cc2b186b2d9e756917c34
HugoAhoy <zhjd3302@outlook.com>
2021-11-04 00:17:56 +0800

    add Karatsuba header

---
+++ include/Karatsuba.hpp
@@ -0,0 +1,7 @@
+// Karatsuba.hpp
+#ifndef KARATSUBA_HPP
+#define KARATSUBA_HPP
+#include "vector"
+
+std::vector<long long> Karatsuba(std::vector<long long> a, std::vector<long long> b, long long base=9);
+#endif
