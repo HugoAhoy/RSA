848eae4a2c4623c6ed1527f6ec74f4bb585005a4
HugoAhoy <zhjd3302@outlook.com>
2021-11-06 09:43:53 +0800

    ADD small prime check before normal miller_rabin to speed up prime check

--- src/util.cpp
+++ src/util.cpp
@@ -9,6 +9,9 @@ std::vector<long long> test_num({2,325,9375,28178,450775,9780504,1795265022});
 
 static std::vector<Int> big_test_num({Random_Nbits_Int(20),Random_Nbits_Int(40),Random_Nbits_Int(60),Random_Nbits_Int(80),Random_Nbits_Int(100),Random_Nbits_Int(120)});
 
+// 1000以下的质数(不包含2,3,5) 150个
+long long small_primes[] = {7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,7173,79,83,89,97,101,103,107,109,113127,131,137,139,149,151,157,163,167,173179,181,191,193,197,199,211,223,227,229233,239,241,251,257,263,269,271,277,281283,293,307,311,313,317,331,337,347,349353,359,367,373,379,383,389,397,401,409419,421,431,433,439,443,449,457,461,463467,479,487,491,499,503,509,521,523,541547,557,563,569,571,577,587,593,599,601607,613,617,619,631,641,643,647,653,659661,673,677,683,691,701,709,719,727,733739,743,751,757,761,769,773,787,797,809811,821,823,827,829,839,853,857,859,863877,881,883,887,907,911,919,929,937,941947,953,967,971,977,983,991,997};
+
 Int Random_Nbits_Int(long long nbits) {
     static std::random_device rd;
     static std::mt19937 mt(rd());
@@ -83,9 +86,16 @@ Int Random_Nbits_Prime(long long nbits){
 }
 
 bool Miller_Rabin(const Int &b){
-    if(!b.is_odd() || !b.mod3()){
+    if(!b.is_odd() || !b.mod3()||!b.mod5()){
         return false;
     }
+    // 预先对小素数取模筛一遍(不属于miller_rabin原本的内容)
+    for(int i = 0; i < 150; i++){
+        if(b.mod_ll(small_primes[i]).isZero()){
+            return false;
+        }
+    }
+    // std::cout << b.val() << std::endl;
     Int m = b-1;
     Int pos_one(1), neg_one(m);
     long long t = 0;
