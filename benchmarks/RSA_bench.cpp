f2f9d2ee08f5e89cea6f47f4e5dd7aba2c67a583
HugoAhoy <zhjd3302@outlook.com>
2021-11-06 10:23:01 +0800

    ADD benchmark for RSA keygen

--- benchmarks/RSA_bench.cpp
+++ benchmarks/RSA_bench.cpp
@@ -16,3 +16,19 @@ static void RSA_256bits_KeyGen(benchmark::State& state) {
     }
 }
 BENCHMARK(RSA_256bits_KeyGen);
+
+// 512-bit密钥生成
+static void RSA_512bits_KeyGen(benchmark::State& state) {
+    for (auto _ : state){
+        GenerateKey(512);
+    }
+}
+BENCHMARK(RSA_512bits_KeyGen);
+
+// 768-bit密钥生成
+static void RSA_768bits_KeyGen(benchmark::State& state) {
+    for (auto _ : state){
+        GenerateKey(768);
+    }
+}
+BENCHMARK(RSA_768bits_KeyGen);
